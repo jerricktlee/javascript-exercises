c4a044b32f148268b8ed994816c2774e44ac3ab1
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-19 12:10:24 -0800

    Complete exercise 07_tempConversion

--- 07_tempConversion/solution/tempConversion-solution.js
+++ 07_tempConversion/solution/tempConversion-solution.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function (fahrenheit) {
-  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+  return Math.round((fahrenheit - 32) * (5 / 9), 1);
 };
 
 const convertToFahrenheit = function (celsius) {
-  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+  return Math.round(((celsius * 9) / 5 + 32), 1);
 };
 
 module.exports = {
