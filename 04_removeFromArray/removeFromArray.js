a2a6d220db0cb2c133f0fff06b1aa7939611f4b4
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-19 11:25:26 -0800

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...filterVals) {
+   for (let i = 0; i < filterVals.length; i++) {
+      array = array.filter((arrayElement) => arrayElement !== filterVals[i]);
+   }
+   return array;
 };
 
 // Do not edit below this line
