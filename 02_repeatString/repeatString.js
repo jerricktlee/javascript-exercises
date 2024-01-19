f04f9ec8e195e865ad3e700af43f2470c4fe2da5
jerricktlee <83623094+jerricktlee@users.noreply.github.com>
2024-01-19 09:45:03 -0800

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+   if (times < 0) {
+      return 'ERROR';
+   }
+   let repeatedStr = '';
+   for (let i = 0; i < times; i++) {
+      repeatedStr += str;
+   }
+   return repeatedStr;
 };
 
 // Do not edit below this line
